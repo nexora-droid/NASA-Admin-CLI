bfc3c9a73349b072e91940e0dcef9095878b8b86
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2025-11-09 00:42:27 +0800

    After 1hr of brain damage: rewritten firebase into JSON, had to rewrite all commands and also added a landed status

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import hashlib
 import getpass
 import pwinput
 import time
-import firebase_admin
-from firebase_admin import firestore, credentials
+import json
+import sys
+#import firebase_admin
+#from firebase_admin import firestore, credentials
 from datetime import datetime, timedelta
 
 systemname = ""
 active = True
-USER_FILE = "users.txt"
-cred = credentials.Certificate('firebasekey.json')
-firebase_admin.initialize_app(cred)
+USER_FILE = "users.json"
+MISSION_FILE = "missions.json"
+#cred = credentials.Certificate('firebasekey.json')
+#firebase_admin.initialize_app(cred)
 SIM_RATIO = 4
-
-db = firestore.client()
-missions_collection = db.collection("missions")
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
@@ -22,30 +22,32 @@ def hash_password(password):
 def user_exists(username):
     try:
         with open(USER_FILE, "r") as f:
-            for line in f:
-                stored_user, _ = line.strip().split(":")
-                if stored_user == username:
-                    return True
+            users = load_users()
+            return username in users
     except FileNotFoundError:
         return False
-    return False
 
 def check_password(username, password):
     hashed_password = hash_password(password)
     try:
         with open(USER_FILE, "r") as f:
-            for line in f:
-                stored_user, stored_pass = line.strip().split(":")
-                if stored_user == username and stored_pass == hashed_password:
-                    return True
+            user = load_users()
+        return user.get(username) == hashed_password
     except FileNotFoundError:
         return False
-    return False
 
 def add_user(username, password):
     hashed_password = hash_password(password)
-    with open(USER_FILE, "a") as f:
-        f.write(username + ":" + hashed_password + "\n")
+    try:
+        with open(USER_FILE, "r") as f:
+            users = load_users()
+    except FileNotFoundError:
+        users = {}
+    users[username] = hashed_password
+
+    with open(USER_FILE, "w") as f:
+        json.dump(users, f, indent=4)
+
 
 def load():
     max_dots = 3
@@ -72,131 +74,211 @@ def load():
 
 def loginsystem():
     while True:
-        name = input("Login Username: ")
-        if user_exists(name):
-            try:
-                password = pwinput.pwinput(prompt="Password: ")
-            except Exception:
-                password = input("(visible): ")
-            if check_password(name, password):
-                print("Login Successful")
-                global systemname
-                systemname = name
-                system()
-                break
-            else:
-                print("Login Unsuccessful! (Incorrect password/username)")
-        else:
-            print("User does not exist, proceeding to account creation!")
-            while True:
+        try:
+            with open(USER_FILE, "r") as f:
+                pass
+        except FileNotFoundError:
+            with open(USER_FILE, "w") as f:
+                json.dump({}, f)
+        while True:
+            name = input("Login Username: ")
+            if user_exists(name):
                 try:
-                    password = pwinput.pwinput(prompt="Create a Password: ")
-                    confirm = pwinput.pwinput(prompt="Confirm Password: ")
+                    password = pwinput.pwinput(prompt="Login Password:")
                 except Exception:
-                    password = input("(visible) ")
-                    confirm = input("(visible): ")
-                if password != confirm:
-                    print("Passwords do not match, try again!")
-                elif password == "":
-                    print("Password is empty, try again!")
-                else:
-                    add_user(name, password)
-                    print("Account Created Successfully! You can now log in!")
+                    password = input("(visible to you only) ")
+                if check_password(name, password):
+                    print("Login Successful!")
+                    global systemname
+                    systemname = name
+                    system()
                     break
+                else:
+                    print("Login Unsuccessful! Incorrect Password!")
+            else:
+                print("User does not exist, proceeding to account creation!")
+                while True:
+                    try:
+                        password = pwinput.pwinput(prompt="Create a Password:")
+                        confirm = pwinput.pwinput(prompt="Confirm Password:")
+                    except Exception:
+                        password = input("(visible to you only) ")
+                        confirm = input("(visible to you only) ")
+                    if password != confirm:
+                        print("Passwords do not match! Try again!")
+                    elif password == "":
+                        print("Password cannot be empty! Try again!")
+                    else:
+                        add_user(name, password)
+                        print("Account created successfully! You can now login!")
+                        break
+
 def create_mission(name, launch_date):
-    data = {
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+    except FileNotFoundError:
+        missions = {}
+
+    missions[name] = {
         "name": name,
         "launch_date": launch_date,
-        "created_on": datetime.now(),
+        "created_on": datetime.now().isoformat(),
         "user": systemname,
         "launched": False
     }
-    missions_collection.document(name).set(data)
-    print(f"Mission {name} created successfully! Predicted Launch Date: {launch_date}!")
+    with open(MISSION_FILE, "w") as f:
+        json.dump(missions, f, indent=4)
+
+    print(f"\nMission Created Successfully! Predicted launch date{launch_date}")
+
 
 def list_mission():
-    docs = missions_collection.stream()
-    three_days_ago = datetime.now()-timedelta(days=3)
-    docs = missions_collection.where("created_on", ">=", three_days_ago).stream()
-    print("\nMissions from last 3 days:\n")
-    count = 0
-    for doc in docs:
-        mission = doc.to_dict()
-        print(f"- {mission['name']}, --> Predicted Launch Date: {mission['launch_date']}, --> By User: {mission['user']}")
-        count += 1
-    if count == 0:
-        print("No missions created in the last 3 days.")
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+    except FileNotFoundError:
+        print("No Missions Found!")
+        return
 
+    if not missions:
+        print("No Missions Found!")
+        return
+
+    print("All missions:")
+    for mission_name, missions in missions.items():
+        print(f"- Mission: {mission_name} --> Predicted Launch Date: {missions['launch_date']}")
 
 def launch_mission(mission_name):
-    doc_ref = missions_collection.document(mission_name)
-    doc = doc_ref.get()
-    if not doc.exists:
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+    except FileNotFoundError:
+        print("No Missions Found!")
+
+    if mission_name not in missions:
         print(f"Mission {mission_name} does not exist")
         return
-    mission = doc.to_dict()
-    if mission["launched"] == True:
-        print(f"Mission {mission_name} is already launched!")
+    mission = missions[mission_name]
+    if mission.get("launched", False):
+        print(f"Mission {mission_name} already launched!")
         return
-    start_time = datetime.now()
-    doc_ref.update({
-        "launched": True,
-        "start_time": start_time,
-        "progress": 0,
-        "fuel": 100
-    })
-    print(f"Mission {mission_name} launched!")
+
+    mission["launched"] = True
+    mission["start_time"] = datetime.now().isoformat()
+    mission["progress"] = 0
+    mission["fuel"] = 100
+
+    missions[mission_name] = mission
+
+    with open(MISSION_FILE, "w") as f:
+        json.dump(missions, f, indent=4)
+
+    print(f"Mission: {mission_name} launched!")
 
 def check_progress(mission_name):
-    doc_ref = missions_collection.document(mission_name)
-    doc = doc_ref.get()
-    if not doc.exists:
-        print(f"Mission {mission_name} does not exist")
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+            mission = missions.get(mission_name)
+    except FileNotFoundError:
+        print("No Missions Found!")
         return
-    mission=doc.to_dict()
-    if mission["user"] != systemname:
-        print(f"You can only view your own missions!")
+
+    if missions.get("launched", False):
+        print("Mission has not been launched yet!")
         return
-    if not mission.get("launched"):
-        print(f"Mission has not been launched yet!")
-        return
-    start_time = mission.get('start_time')
-    if hasattr(start_time, "to_datetime"):
-        start_time = start_time.to_datetime()
+
+    start_time = datetime.fromisoformat(mission['start_time'])
     elapsed_realminutes = (datetime.now() - start_time).total_seconds()/60
     flight_minutes = elapsed_realminutes * SIM_RATIO
     total_flighttime = 120
     progress = min((flight_minutes / total_flighttime)*100, 100)
     fuel = max(100 - progress, 0)
-    print(f'Mission Info\n'
-          f'Mission Name: {mission_name}\n'
-          f'Progress: {progress:.2f}%\n'
-          f'Fuel Remaining: {fuel:.2f}%\n')
-    doc_ref.update({
-        "progress": progress,
-        "fuel": fuel
-    })
+    mission["progress"] = progress
+    mission["fuel"] = fuel
+
+    if progress >= 100:
+        mission["landed"] = True
+        print(f"Mission {mission_name}\n"
+              f"Progress: 100%\n"
+              f"Fuel Remaining: 0%\n"
+              f"Status: Landed!")
+    else:
+        print(f'Mission Info\n'
+              f'Mission Name: {mission["name"]}\n'
+              f'Progress: {progress:.2f}%\n'
+              f'Fuel Remaining: {fuel:.2f}%\n')
+
+    missions[mission_name] = mission
+    with open(MISSION_FILE, "w") as f:
+        json.dump(missions, f, indent=4)
+
 
 def delete_mission(mission_name):
-    doc_ref = missions_collection.document(mission_name)
-    if not doc_ref.get().exists:
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+            mission = missions.get(mission_name)
+    except FileNotFoundError:
+        print("Mission not found!")
+    if mission_name not in missions:
         print(f"Mission {mission_name} does not exist")
         return
-    doc_ref.delete()
+    del missions[mission_name]
+    with open(MISSION_FILE, "w") as f:
+        json.dump(missions, f, indent=4)
     print(f"Mission {mission_name} deleted!")
 
+
 def summary(mission_name):
-    docs = missions_collection.where("user", "==", systemname).stream()
-    for docs in docs:
-        mission = docs.to_dict()
-        print(f"Mission {mission['name']}, --> By User: {mission['user']}, --> Launched: {mission['launched']}")
-def update(mission_name, new_date):
-    doc_ref = missions_collection.document(mission_name)
-    if not doc_ref.get().exists:
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+    except FileNotFoundError:
+        print("Mission not found!")
+        return
+    if mission_name not in missions:
         print(f"Mission {mission_name} does not exist")
         return
-    doc_ref.update({"launch_date": new_date})
-    print(f"Mission date changed to {new_date}")
+    mission = missions[mission_name]
+    print(f"Mission: {mission_name} --> Launch Date: {mission['launch_date']} --> User: you! --> Launched: {mission['launched']}")
+def update(mission_name, new_date):
+    try:
+        with open(MISSION_FILE, "r") as f:
+            missions = load_mission()
+    except FileNotFoundError:
+        print("Mission not found!")
+        return
+    if mission_name not in missions:
+        print(f"Mission {mission_name} does not exist")
+        return
+
+    mission = missions[mission_name]
+    mission["launch_date"] = new_date
+    missions[mission_name] = mission
+    with open(MISSION_FILE, "w") as f:
+        json.dump(missions, f, indent=4)
+
+def load_mission():
+    try:
+        with open(MISSION_FILE, "r") as f:
+            content = f.read().strip()
+            if not content:
+                return {}
+            return json.loads(content)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+def load_users():
+    try:
+        with open(USER_FILE, "r") as f:
+            content = f.read().strip()
+            if not content:
+                return {}
+            return json.loads(content)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
 def system():
     isOn = True
     print("Welcome Admin! Type help to view list of available commands")
@@ -216,7 +298,7 @@ def system():
                   "launch\n"
                   "\tLaunches mission (user-specific only)\n"
                   "list_missions\n"
-                  "\tLists missions created by global users in the past 3 days\n"
+                  "\tLists missions created by you!\n"
                   "progress\n"
                   "\tCheck progress of your mission\n"
                   "delete\n"
@@ -227,6 +309,7 @@ def system():
                   "\tUpdates predicated launch date")
         if commandline == "exit":
             isOn = False
+            sys.exit()
         elif commandline == "status":
             global active
             print(f"Active: {active}")
@@ -246,6 +329,12 @@ def system():
             mission_name = input("Mission name: ")
             new_date = input("New mission date: ")
             update(mission_name, new_date)
+        elif commandline == "summary":
+            mission_name = input("Mission name: ")
+            summary(mission_name)
+        elif commandline == "progress":
+            mission_name = input("Mission name: ")
+            check_progress(mission_name)
 if __name__ == "__main__":
     load()
     loginsystem()
